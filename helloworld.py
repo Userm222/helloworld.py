f9ca175cc020ce998c29607578de83967c0f2b65
Userm222 <168639164+Userm222@users.noreply.github.com>
2024-05-01 16:45:17 +0530

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,37 @@
+# # Function
+
+# f(x) = x^2, f(3)=9, f(4) = 16
+# g(x) = 3*x, g(10) = 30
+
+# import math
+
+# def squared_difference(x,y):
+#     return (x-y)*(x-y)
+
+# def distance(x1,x2,y1,y2):
+#     term1 = squared_difference(x1,x2)
+#     term2 = squared_difference(y1,y2)
+#     return math.sqrt(term1 + term2)
+
+
+# print(math)
+
+# Given a list of points (X,Y)
+# Find the perimeter of the polygon formed
+
+# Can you write a code to calculate the n-th Fibonacci number.
+# F(n) = F(n-1) + F(n-2), F(0) = 0, F(1) = 1, n >= 0
+
+# Recursion
+
+# def recursive(a):
+#     if a == 1:
+#         return a
+#     else:
+#         return recursive(a-1) + 5 # 1 + 5 + 5 = 11
+    
+# print(recursive(3))
+
+# # Lambda
+# x = lambda a : a + 10
+# print(x(5))
